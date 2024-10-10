2d2e1a849c38c079599d9cd94d1ecb6b8078ebdd
Fathimah Azzahra Winarno <fathimahazzahra425@gmail.com>
2024-10-10 11:16:58 +0700

    update

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,14 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>Halo DUnia</h1>
-</body>
-
-</html>
+<x-layout>
+    <x-slot:title>{{ $title }}</x-slot>
+        <h3 class="text-xl;">Ayo kembangkan Karirmu di PT Taharica</h3>
+        <a href="https://taharica.co.id/">Baca selengkapnya &raquo;</a>
+</x-layout>
